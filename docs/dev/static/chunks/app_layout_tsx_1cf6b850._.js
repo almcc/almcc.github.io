17d1a8b95158ff0d767eda3cad390cb5cfe758f4
Alastair McClelland <alastair.mcclelland@gmail.com>
2026-03-06 22:11:54 +0000

    chore: remove .DS_Store files, update .gitignore, and rebuild docs

--- docs/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_af2bc2ff._.js",
-  "static/chunks/app_7a051fc1._.js",
-  "static/chunks/app_globals_71f961d1.css"
-],
-    source: "dynamic"
-});
